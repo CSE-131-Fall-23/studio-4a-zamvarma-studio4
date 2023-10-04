f3b076ca638ee95ae4909ffae0ccfe880a9f5b5c
joshua <joshua@10.232.168.24>
2023-10-04 15:45:46 -0500

    complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,25 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.rectangle(0.5, 0.5, .4, .25);
 		
+		double[]x = new double[]{0.1, 0.1, 0.9};
+		double[]y = new double[]{0.25, 0.5, 0.25};
+		
+		StdDraw.setPenColor(154,207,97);
+		StdDraw.filledPolygon(x, y);
+		
+		double[]x2 = new double[]{0.5, 0.9, 0.9};
+		double[]y2 = new double[]{0.75, 0.25, 0.75};
+		
+		StdDraw.setPenColor(230, 199, 44);
+		StdDraw.filledPolygon(x2, y2);
+		
+		StdDraw.setPenColor(154,207,97);
+		StdDraw.filledRectangle(.5, .5, .07, .07);
+		
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledCircle(.5, .5, .07);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -8,7 +8,7 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
-
+/// String shapeType, int redComponent, int greenComponent, int blueComponent, boolean isFilled, double parameterOne, double parameterTwo, double parameterThree
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
@@ -19,6 +19,53 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double paramenterOne = in.nextDouble();
+		double paramenterTwo = in.nextDouble();
+		double paramenterThree = in.nextDouble();
+		double paramenterFour = in.nextDouble();
+		
+		double paramenterFive = 0;
+		double paramenterSix = 0;
+		
+		if (shapeType.equals("triangle")) {
+			paramenterFive = in.nextDouble();
+			paramenterSix = in.nextDouble();
+		}
+		
+		double[]x = new double[]{paramenterOne, paramenterThree, paramenterFive};
+		double[]y = new double[]{paramenterTwo, paramenterFour, paramenterSix};
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		//redComponent, greenComponent, blueComponent, isFilled,
+		System.out.println(shapeType);
+		if (shapeType.equals("rectangle") && !isFilled) {
+			StdDraw.rectangle( paramenterOne, paramenterTwo, paramenterThree, paramenterFour);	
+		}
+		else if(shapeType.equals("rectangle")) {
+			StdDraw.filledRectangle(paramenterOne, paramenterTwo, paramenterThree, paramenterFour);
+		}
+		else if(shapeType.equals("ellipse") && !isFilled) {
+			StdDraw.ellipse(paramenterOne, paramenterTwo, paramenterThree, paramenterFour);
+		}
+		else if(shapeType.equals("ellipse")) {
+			StdDraw.filledEllipse(paramenterOne, paramenterTwo, paramenterThree, paramenterFour);
+		}
+
+		else if(shapeType.equals("triangle") && !isFilled) {
+			StdDraw.polygon(x,y);
+		}
+		else if(shapeType.equals("triangle")) {
+			StdDraw.filledPolygon(x,y);
+		}
+		
+		
+		//System.out.println(shapeType+redComponent);
 		
 	}
 }
